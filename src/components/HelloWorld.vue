bf20175ff09a9eaf450207bac2020d5034afcbc1
Greg MacGregor <gmacgregor@gmail.com>
2018-08-06 23:32:31 -0400

    Inial alpha commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,61 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    
+    <div v-if="$apollo.loading">Apollo is loading...</div>
+    <div v-if="$apollo.queries.posts.loading">Posts are Loading...</div>
+
+    <section v-if="posts">
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          <!-- <router-link :to="`/post/${post.slug}`" class="link"> -->
+            <div class="placeholder">
+              <!-- <img
+                :alt="post.title"
+                :src="`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${post.coverImage.handle}`"
+              /> -->
+            </div>
+            <h3>{{post.title}}</h3>
+          <!-- </router-link> -->
+        </li>
+      </ul>
+      <!-- <button v-if="postCount && postCount > allPosts.length" @click="loadMorePosts">
+        {{loading ? 'Loading...' : 'Show more'}}
+      </button> -->
+    </section>
+    <h2 v-else>No posts</h2>
   </div>
 </template>
 
 <script>
+import gql from "graphql-tag";
+
+const allPosts = gql`{
+  posts(orderBy: dateAndTime_DESC) {
+    title,
+    dateAndTime,
+    slug,
+    id,
+    text
+  }
+}`;
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: () => ({
+    loading: 0,
+    posts: null
+  }),
+  apollo: {
+    $loadingKey: 'loading',
+    posts: allPosts,
+    variables: {
+      skip: 0,
+      first: 0
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
+
+import App from "./App.vue"
 
 Vue.config.productionTip = false
 
+const GRAPH_CMS_API = "https://api-useast.graphcms.com/v1/cjkhrc6u40y5c01gmlpvsny01/master";
+
+const apolloClient = new ApolloClient({
+  link: new HttpLink({ uri: GRAPH_CMS_API }),
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+});
+
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  provide: apolloProvider.provide()
+}).$mount("#app")
